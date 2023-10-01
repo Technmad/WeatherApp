a57588200a035e10b301739e28d39be763415e82
anurag <anuragpathakoffl@gmail.com>
2023-10-01 05:39:31 +0530

    added giffy api

--- app.js
+++ app.js
@@ -16,6 +16,9 @@ async function getWeatherInfo(city) {
 function displayData(data) {
   setTemperature(data);
   setLocation(data);
+  setForcastData(data);
+  //   console.log(data.current.condition.text);
+  getGif(data.current.condition.text);
 }
 
 function setTemperature(data) {
@@ -29,6 +32,37 @@ function setTemperature(data) {
 function setLocation(data) {
   document.querySelector(".city").innerHTML = data.location.name;
   document.querySelector(".country").innerHTML = data.location.country;
+}
+
+function setForcastData(data) {
+  document.querySelector(".sunrise h5").innerHTML =
+    data.forecast.forecastday[0].astro.sunrise;
+  document.querySelector(".sunset h5").innerHTML =
+    data.forecast.forecastday[0].astro.sunset;
+  document.querySelector(".maxtemp h5").innerHTML =
+    data.forecast.forecastday[0].day.maxtemp_c + "°C";
+  document.querySelector(".mintemp h5").innerHTML =
+    data.forecast.forecastday[0].day.mintemp_c + "°C";
+  document.querySelector(".maxwind h5").innerHTML =
+    data.forecast.forecastday[0].day.maxwind_kph + "km/h";
+  document.querySelector(".humidity h5").innerHTML =
+    data.forecast.forecastday[0].day.avghumidity + "%";
+  document.querySelector(".precipitation h5").innerHTML =
+    data.forecast.forecastday[0].day.totalprecip_mm + "mm";
+  document.querySelector(".uv h5").innerHTML =
+    data.forecast.forecastday[0].day.uv;
+}
+
+async function getGif(value) {
+  console.log(value);
+  const result = await fetch(
+    `https://api.giphy.com/v1/stickers/translate?api_key=U6CX6e7472i4Z2zdYsg4DWM4rd4QKfe7&s=` +
+      value,
+    { mode: "cors" }
+  );
+
+  response = await result.json();
+  document.querySelector(".gif img").src = response.data.images.original.url;
 }
 
 searchBttn.addEventListener("click", () => {
